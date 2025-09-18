a5bbea80efee1b91238396d9b793d79bad01c6bc
NikoMalik <NikoMalik@users.noreply.github.com>
2025-09-18 21:42:47 +0300

    bool to int

--- asm_amd64.s
+++ asm_amd64.s
@@ -1,11 +1,11 @@
 #include "textflag.h"
 #include "go_asm.h"
 
-#define    get_tls(r)    MOVQ TLS, r
-#define    g(r)    0(r)(TLS*1)
+#define get_tls(r)    MOVQ TLS, r
+#define g(r)    0(r)(TLS*1)
 
-TEXT ·GetG(SB),NOSPLIT,$0-8
-    get_tls(CX)
-    MOVQ    g(CX), AX
-    MOVQ    AX, gp+0(FP)
-    RET
+TEXT ·GetG(SB), NOSPLIT, $0-8
+	get_tls(CX)
+	MOVQ g(CX), AX
+	MOVQ AX, gp+0(FP)
+	RET
--- utils.go
+++ utils.go
@@ -454,6 +454,13 @@ func EncodeUnrolled8(dst, src []byte) int {
 	return j
 }
 
+// Bool2int returns 0 if x is false or 1 if x is true.
+func Bool2int(x bool) int {
+	// Avoid branches. In the SSA compiler, this compiles to
+	// exactly what you would want it to.
+	return int(*(*uint8)(unsafe.Pointer(&x)))
+}
+
 func MulUintptr(a, b uintptr) (uintptr, bool) {
 	if a|b < 1<<(4*PtrSize) || a == 0 {
 		return a * b, false
